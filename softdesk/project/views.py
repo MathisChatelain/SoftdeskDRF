1009145311117561ec2e668fd8cd4efb0f867a3b
Mathis Chatelain <mathis.chatelain@socotec.com>
2023-08-06 17:49:33 +0200

    rm pycache

--- softdesk/project/views.py
+++ softdesk/project/views.py
@@ -4,69 +4,10 @@ from project.serializers import (
     ProjectSerializer,
     CustomUserSerializer,
     ContributorSerializer,
-    CustomUserSignupSerializer,
     IssueSerializer,
     CommentSerializer,
 )
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
-
-from django.contrib.auth import authenticate, login, logout
-from django.shortcuts import redirect, render
-
-
-def login_page(request):
-    form = CustomUserSerializer()
-    message = ""
-    if request.method == "POST":
-        form = CustomUserSerializer(data=request.POST)
-        if form.is_valid():
-            user = authenticate(
-                username=form.cleaned_data["username"],
-                password=form.cleaned_data["password"],
-            )
-            custom_user = CustomUser.objects.get(user=user)
-            if user is not None:
-                login(request, user)
-                return redirect("project")
-        message = "Identifiants invalides."
-    return render(
-        request, "authentication/login.html", context={"form": form, "message": message}
-    )
-
-
-def signup(request):
-    form = CustomUserSignupSerializer()
-    message = ""
-    if request.method == "POST":
-        form = CustomUserSignupSerializer(data=request.POST)
-        if form.is_valid():
-            # We authenticate the user to log him in if the account already exists
-            user = authenticate(
-                username=form.cleaned_data["username"],
-                password=form.cleaned_data["password"],
-            )
-            if user is not None:
-                login(request, user)
-                return redirect("home")
-            else:
-                # We create the user if it does not exist
-                user = CustomUser.objects.create_user(
-                    username=form.cleaned_data["username"],
-                    password=form.cleaned_data["password"],
-                )
-                user.save()
-                login(request, user)
-                return redirect("home")
-    return render(
-        request,
-        "authentication/signup.html",
-        context={"form": form, "message": message},
-    )
-
-
-def logout_user(request):
-    logout(request)
-    return redirect("login")
 
 
 class ProjectViewset(ModelViewSet):
